68f7d77406f2290bab4f9b374ddf5b9665d43040
Alexey Parfenov <alepar@gmail.com>
2012-11-10 21:36:52 +0400

    formatting

--- src/java/main/ru/alepar/battletanks/engine/opengl/OpenGLFrame.java
+++ src/java/main/ru/alepar/battletanks/engine/opengl/OpenGLFrame.java
@@ -21,33 +21,33 @@ class OpenGLFrame implements Frame {
     private int createTankList() {
         final int lTank = gl.glGenLists(1);
         gl.glNewList(1, GL2.GL_COMPILE);
-        gl.glLineWidth(2.0f);
+            gl.glLineWidth(2.0f);
 
-        // bounding square
-        gl.glBegin(GL2.GL_LINE_LOOP);
-        gl.glVertex3i(-5, -5, 0);
-        gl.glVertex3i(-5, 5, 0);
-        gl.glVertex3i(5, 5, 0);
-        gl.glVertex3i(5, -5, 0);
-        gl.glEnd();
+            // bounding square
+            gl.glBegin(GL2.GL_LINE_LOOP);
+                gl.glVertex3i(-5, -5, 0);
+                gl.glVertex3i(-5,  5, 0);
+                gl.glVertex3i( 5,  5, 0);
+                gl.glVertex3i( 5, -5, 0);
+            gl.glEnd();
 
-        // gun
-        gl.glBegin(GL2.GL_LINES);
-        gl.glVertex3i(0, 0, 0);
-        gl.glVertex3i(5, 0, 0);
-        gl.glEnd();
+            // gun
+            gl.glBegin(GL2.GL_LINES);
+                gl.glVertex3i(0, 0, 0);
+                gl.glVertex3i(5, 0, 0);
+            gl.glEnd();
 
-        // bottom track
-        gl.glBegin(GL2.GL_LINES);
-        gl.glVertex3i(-5, -3, 0);
-        gl.glVertex3i(5, -3, 0);
-        gl.glEnd();
+            // bottom track
+            gl.glBegin(GL2.GL_LINES);
+                gl.glVertex3i(-5, -3, 0);
+                gl.glVertex3i( 5, -3, 0);
+            gl.glEnd();
 
-        // top track
-        gl.glBegin(GL2.GL_LINES);
-        gl.glVertex3i(-5, 3, 0);
-        gl.glVertex3i(5, 3, 0);
-        gl.glEnd();
+            // top track
+            gl.glBegin(GL2.GL_LINES);
+                gl.glVertex3i(-5, 3, 0);
+                gl.glVertex3i( 5, 3, 0);
+            gl.glEnd();
         gl.glEndList();
         return lTank;
     }
